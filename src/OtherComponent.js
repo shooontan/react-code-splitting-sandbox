710a04ebc24d151ae85fd081aa6db2f95b0915a0
shooontan <logo.bee.doo19@gmail.com>
2018-02-22 10:10:02 +0900

    react-loadable

---
+++ src/OtherComponent.js
@@ -0,0 +1,6 @@
+// @flow
+import * as React from 'react';
+
+const OtherComponent = () => <p>This is OtherComponent</p>;
+
+export default OtherComponent;
--- src/app.js
+++ src/app.js
@@ -2,11 +2,35 @@
 /* eslint-disable no-console */
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
+import Loadable from 'react-loadable';
 
 import(/* webpackChunkName: "math" */ './math').then((math) => {
   console.log(math.add(16, 26));
 });
 
-const App = () => <div>hello splitted world</div>;
+const Loading = (props: { error: ?Error, pastDelay: boolean, timedOut: boolean }) => {
+  if (props.error) {
+    return <div>Error!</div>;
+  } else if (props.timedOut) {
+    return <div>Taking a long time...</div>;
+  } else if (props.pastDelay) {
+    return <div>Loading...</div>;
+  }
+  return null;
+};
+
+const LoadableOtherComponent = Loadable({
+  loader: () => import(/* webpackChunkName: "OtherComponent" */ './OtherComponent'),
+  loading: Loading,
+  delay: 1000,
+  timeout: 100,
+});
+
+const App = () => (
+  <React.Fragment>
+    <div>hello splitted world</div>
+    <LoadableOtherComponent />
+  </React.Fragment>
+);
 
 export default hot(module)(App);
